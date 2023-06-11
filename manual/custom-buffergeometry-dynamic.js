c0974ca92a66fdede0d757231f63e469fdb87952
linyuan <673609957@qq.com>
2023-06-11 19:40:25 +0800

    html:buffergeometry

---
+++ manual/custom-buffergeometry-dynamic.js
@@ -0,0 +1,99 @@
+import * as THREE from 'three'
+import GUI from 'lil-gui';
+import {OrbitControls} from 'three/addons/controls/OrbitControls';
+
+const gui = new GUI();
+
+function main(){
+
+  const canvas = document.querySelector('#c')
+  const renderer = new THREE.WebGLRenderer({canvas})
+  function makeCamera (fov=75) {
+    const aspect = 2
+    const zNear = 0.1
+    const zFar = 100
+    // 创建一个透视相机（视锥体）
+    return new THREE.PerspectiveCamera(fov,aspect,zNear,zFar)
+  }
+  const camera = makeCamera()
+  camera.position.z = 3
+  // 相机朝向
+  camera.lookAt(0,0,0)
+
+  // 建立一个场景节点
+  const scene = new THREE.Scene()
+
+  function addLight(...pos){
+    const color = 0xffffff
+    const intensity = 1
+    const light = new THREE.DirectionalLight(
+      color,intensity
+    )
+    light.position.set(...pos)
+    scene.add(light)
+  }
+  addLight(-1,2,4)
+  addLight(-1,2,4)
+
+  function makeSpherePosition(segmentsAround,segmentsDown){
+    const numVertices = segmentsAround*segmentsDown*6
+  }
+
+
+
+
+  const controls = new OrbitControls(camera,canvas)
+  // controls.target.set(0, 5, 0);
+  controls.update();
+  // 在场景中添加光源
+  {
+    // 创建平行光
+    const light = new THREE.DirectionalLight(0x000fff,1)
+    //设置光源位置
+    light.position.set(0,20,0)
+    scene.add(light)
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  function render(time){
+    time *= 0.001
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    renderer.setSize(width, height, false);
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(render)
+  }
+  requestAnimationFrame(render)
+}
+main()
