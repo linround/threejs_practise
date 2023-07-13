9c47a8a5e966ecebe0c2c77b9f5af7de02525b26
linyuan <673609957@qq.com>
2023-07-13 15:11:00 +0800

    fix:add links

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,10 @@ const  fs = require('fs')
 // 对于某些资源文件，需要复制到对应路径下
 const paths = {}
 function getFiles(fold){
+  // 读取文件夹下的所有文件
   const files = fs.readdirSync(fold)
   const htmlReg = /(.+(?=[.html]$))/
+  // 匹配得到html文件
   const htmlFile = files.filter(file =>{
     return htmlReg.test(file)
   })
